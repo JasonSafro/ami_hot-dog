78f0c6f380d01b671be0f7a1614ad18cab702b5d
Josh <josh@interactivestrategies.com>
2014-06-21 21:38:21 -0400

    beginning theming of homepage/press release landing and detail pages.
    6/21 database dump

--- docroot/sites/all/themes/hotdogs/templates/pages/page--front.tpl.php
+++ docroot/sites/all/themes/hotdogs/templates/pages/page--front.tpl.php
@@ -111,7 +111,32 @@
     </section>
     <!--/.l-help -->
   <?php endif; ?>
+  
+  <!-- Hero Area -->
+  <section class="full-screen-photo checks" id="features-hero">
+      <div class="row bkgrd-white roundcrnr">
+          <div class="small-12 medium-4 large-4 column">
+              <h1 class="main-header">Facts Figures &amp; Folklore</h1><br>
+              <a class="large radius button" href="#">Get Some Facts!</a><br>
+          </div>
+          <div class="medium-8 large-8 columns">
+              <img src="<?php echo base_path() . path_to_theme(); ?>/images/large-image-dog.jpg">
+          </div>
+      </div>
+  </section>
+	<!-- End Hero Area -->
 
+	<!-- Homepage Banner -->
+  <div class="bkgrd-lightred">
+      <div class="row">
+          <div class="large-12 columns text-center">
+              <h2 class="text-white gutter-top">Hot dog and sausage statistics, recipes, films that have featured hot dogs and more.</h2>
+              <p class=" lead hide-for-small text-white">We have the skinny on all of the latest facts and trivia, you name it we have it.</p>
+          </div>
+      </div>
+  </div>
+ 	<!-- End Homepage Banner --> 
+  
   <main role="main" class="row l-main">
     <div class="<?php print $main_grid; ?> main columns">
       <?php if (!empty($page['highlighted'])): ?>
@@ -203,18 +228,73 @@
   <?php endif; ?>
 
   <!--.l-footer-->
-  <footer class="l-footer panel row" role="contentinfo">
-    <?php if (!empty($page['footer'])): ?>
-      <div class="footer large-12 columns">
-        <?php print render($page['footer']); ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
-    <?php endif; ?>
+  <footer class="l-footer" role="contentinfo">
+    <div class="row gutter-bottom-lg">
+        <h2 class="dotted-header"><span>Connect With Us</span></h2>
+        <div class="medium-12 medium-centered medium-offset-3 small-12 small-centered columns" >
+            <div id="social">
+            <p><a class="youtube" href="http://www.youtube.com/user/hotdogcouncil">YouTube</a></p>
+                <p><a class="facebook" href="https://www.facebook.com/NHDSC">Facebook</a></p>
+            <p><a class="pinterest" href="http://localhost/dev.hot-dog.local/designs/nhdsc-v1/index.php#">Pinterst</a></p>
+        <p><a class="allrecipes" href="http://allrecipes.com/cook/natlhotdogsausagecounci/">All Recipes</a></p>
+    <p><a class="zazzle" href="http://www.zazzle.com/hotdogcouncil">Zazzle</a></p>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    <div class="bkgrd-creme">
+        <div class="row gutter-top-lg">
+            <div class="small-12 medium-6 large-6 columns push-6 text-grey sans">
+                <h4 class="text-hotdogred">Media Contacts</h4>
+                <p class="small text-light">Janet Riley<br>
+                    President, NHDSC and 'Queen of Wien'<br>
+                    202/587-4245, cell 703/801-2238<br>
+                    <a href="mailto:jriley@meatami.com" class="text-grey">jriley@meatami.com</a></p>
+    
+                <p class="small">Eric Mittenthal<br>
+                    Vice President, Public Affairs<br>
+                    National Hot Dog &amp; Sausage Council<br>
+                    202/587-4238, cell 404/808-8396<br>
+                    <a href="mailto:emittenthal@meatami.com" class="text-grey">emittenthal@meatami.com</a></p>
+            </div>
+            <div class="small-12 medium-6 large-6 columns pull-6 text-grey sans ">
+                <p><img src="<?php echo base_path() . path_to_theme(); ?>/images/nhdsc-logo-newest-dark.png" alt="National Hot Dog &amp; Sausage Council"></p>
+                <p class="small"> 1150 Connecticut Avenue, NW, 12th floor <br>
+                    Washington, DC&nbsp;&nbsp; | 202-587-4200 <br>
+                </p>
+    
+                <ul class="inline-list">
+                    <li><a href="http://allrecipes.com/cook/natlhotdogsausagecounci/" class="text-grey ">All Recipes</a></li>
+                    <li><a href="https://www.facebook.com/NHDSC" class="text-grey">Facebook</a></li>
+                    <li><a href="http://www.youtube.com/user/hotdogcouncil" class="text-grey">Youtube</a></li>
+                    <li><a href="http://www.pinterest.com/hotdogsausage/" class="text-grey">Pinterest</a></li>
+                </ul>
+    
+                <!--/ .social-icons -->
+            </div>
+        </div>
+    </div>
+    
+    <!-- END Full width -->
+    
+    <!-- Footer -->
+    
+    <div class="bkgrd-hotdogred gutter-bottom">
+        <div class="row">
+            <div class="medium-4 medium-4 push-8 columns gutter-top">
+                <ul class="inline-list smaller">
+                    <li><a href="about.php" class="text-ltgrey ">About</a></li>
+                    <li><a href="#" class="text-ltgrey"> News</a></li>
+                    <li><a href="#" class="text-ltgrey">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="medium-8 medium-8 pull-4 columns gutter-top ">
+                <p class="smaller text-ltgrey">&copy; Copyright, just source credit</p>
+            </div>
+        </div>
+    </div>
   </footer>
   <!--/.footer-->
 
--- docroot/sites/all/themes/hotdogs/templates/pages/page.tpl.php
+++ docroot/sites/all/themes/hotdogs/templates/pages/page.tpl.php
@@ -122,8 +122,6 @@
 
       <a id="main-content"></a>
 
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
-
       <?php if ($title && !$is_front): ?>
         <?php print render($title_prefix); ?>
         <h1 id="page-title" class="title"><?php print $title; ?></h1>
@@ -203,18 +201,73 @@
   <?php endif; ?>
 
   <!--.l-footer-->
-  <footer class="l-footer panel row" role="contentinfo">
-    <?php if (!empty($page['footer'])): ?>
-      <div class="footer large-12 columns">
-        <?php print render($page['footer']); ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
-    <?php endif; ?>
+  <footer class="l-footer" role="contentinfo">
+    <div class="row gutter-bottom-lg">
+        <h2 class="dotted-header"><span>Connect With Us</span></h2>
+        <div class="medium-12 medium-centered medium-offset-3 small-12 small-centered columns" >
+            <div id="social">
+            <p><a class="youtube" href="http://www.youtube.com/user/hotdogcouncil">YouTube</a></p>
+                <p><a class="facebook" href="https://www.facebook.com/NHDSC">Facebook</a></p>
+            <p><a class="pinterest" href="http://localhost/dev.hot-dog.local/designs/nhdsc-v1/index.php#">Pinterst</a></p>
+        <p><a class="allrecipes" href="http://allrecipes.com/cook/natlhotdogsausagecounci/">All Recipes</a></p>
+    <p><a class="zazzle" href="http://www.zazzle.com/hotdogcouncil">Zazzle</a></p>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    <div class="bkgrd-creme">
+        <div class="row gutter-top-lg">
+            <div class="small-12 medium-6 large-6 columns push-6 text-grey sans">
+                <h4 class="text-hotdogred">Media Contacts</h4>
+                <p class="small text-light">Janet Riley<br>
+                    President, NHDSC and 'Queen of Wien'<br>
+                    202/587-4245, cell 703/801-2238<br>
+                    <a href="mailto:jriley@meatami.com" class="text-grey">jriley@meatami.com</a></p>
+    
+                <p class="small">Eric Mittenthal<br>
+                    Vice President, Public Affairs<br>
+                    National Hot Dog &amp; Sausage Council<br>
+                    202/587-4238, cell 404/808-8396<br>
+                    <a href="mailto:emittenthal@meatami.com" class="text-grey">emittenthal@meatami.com</a></p>
+            </div>
+            <div class="small-12 medium-6 large-6 columns pull-6 text-grey sans ">
+                <p><img src="<?php echo base_path() . path_to_theme(); ?>/images/nhdsc-logo-newest-dark.png" alt="National Hot Dog &amp; Sausage Council"></p>
+                <p class="small"> 1150 Connecticut Avenue, NW, 12th floor <br>
+                    Washington, DC&nbsp;&nbsp; | 202-587-4200 <br>
+                </p>
+    
+                <ul class="inline-list">
+                    <li><a href="http://allrecipes.com/cook/natlhotdogsausagecounci/" class="text-grey ">All Recipes</a></li>
+                    <li><a href="https://www.facebook.com/NHDSC" class="text-grey">Facebook</a></li>
+                    <li><a href="http://www.youtube.com/user/hotdogcouncil" class="text-grey">Youtube</a></li>
+                    <li><a href="http://www.pinterest.com/hotdogsausage/" class="text-grey">Pinterest</a></li>
+                </ul>
+    
+                <!--/ .social-icons -->
+            </div>
+        </div>
+    </div>
+    
+    <!-- END Full width -->
+    
+    <!-- Footer -->
+    
+    <div class="bkgrd-hotdogred gutter-bottom">
+        <div class="row">
+            <div class="medium-4 medium-4 push-8 columns gutter-top">
+                <ul class="inline-list smaller">
+                    <li><a href="about.php" class="text-ltgrey ">About</a></li>
+                    <li><a href="#" class="text-ltgrey"> News</a></li>
+                    <li><a href="#" class="text-ltgrey">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="medium-8 medium-8 pull-4 columns gutter-top ">
+                <p class="smaller text-ltgrey">&copy; Copyright, just source credit</p>
+            </div>
+        </div>
+    </div>
   </footer>
   <!--/.footer-->
 
