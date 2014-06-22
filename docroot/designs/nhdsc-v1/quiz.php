56e39e7a564032e7160ca99f7054de60f4f4d13f
Gamble McCown <gamble.mccown@gmail.com>
2014-06-22 07:55:57 -0400

    quiz page

---
+++ docroot/designs/nhdsc-v1/quiz.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>National Hot Dog and Sausage Council | Quiz</title>
+
+    <!--start headandtopnavbar.php-->
+    <?php include('headandtopnavbar.php'); ?>
+    <!--end headandtopnavbar.php-->
+
+
+    <!--ADD NEW STUFF HERE -->
+
+
+<div class="bkgrd-lightred">
+    <div class="row ">
+        <div class="large-12 columns text-center gutter-top">
+            <h1 class="text-white">Quiz</h1>
+
+            <p class="lead hide-for-small text-white">From the National Hot Dog and Sausage Council</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="large-8 medium-8 columns gutter-top">
+
+        <p><img src="http://placehold.it/700x300&text=[img]"/></p>
+
+        <h2>How much do you know about hot dogs? Find out if you’re a hot dog fact-champ or a wiener</h2>
+
+        <div class="quiz">
+            <label class="question">1. Who, when transitioning from the silent screen, uttered “hot dogs” as his or her
+                first ever on- screen words?</label>
+            <input type="radio" name="silentscreen" value="CharlieChaplin" id="CharlieChaplin"><label
+            for="CharlieChaplin" class="answers">Charlie Chaplin</label><br>
+            <input type="radio" name="silentscreen" value="MickeyMouse" id="MickeyMouse"><label for="MickeyMouse"
+                                                                                                class="answers">Mickey
+            Mouse</label><br>
+            <input type="radio" name="silentscreen" value="GretaGarbo" id="GretaGarbo"><label for="GretaGarbo"
+                                                                                              class="answers">Greta
+            Garbo</label><br>
+            <input type="radio" name="silentscreen" value="Blue" id="GrouchoMarx"><label for="GrouchoMarx"
+                                                                                         class="answers">Groucho
+            Marx</label>
+
+            <p class="gutter-top"><a href="#" class="button roundcrnr">Next</a></p>
+
+        </div>
+
+        <div class="quiz">
+            <h2>Right!</h2>
+
+            <p class="question">1. Who, when transitioning from the silent screen, uttered “hot dogs” as his or her first ever on- screen
+                words?
+            <ul class="square answers">
+                <li>Charlie Chaplin</li>
+                <li class="text-heavy lightred">Mickey Mouse</li>
+                <li>Greta Garbo</li>
+                <li>Groucho Marx</li>
+            </ul>
+            <h4 class="text-grey">The first words Mickey Mouse ever uttered in a cartoon were “hot dogs” (in “The
+                Karnival Kid, 1929). The cartoons he was featured in were silent ones up until that point.</h4>
+
+            <div class="gutter-top" ><a href="#" class="button roundcrnr">Next</a></div>
+        </div>
+
+
+
+
+
+        <div class="quiz">
+            <h2>False!</h2>
+
+            <p class="question">1. Who, when transitioning from the silent screen, uttered “hot dogs” as his or her first ever on- screen
+                words?
+            <ul class="square answers">
+                <li>Charlie Chaplin</li>
+                <li class="text-heavy lightred">Mickey Mouse</li>
+                <li>Greta Garbo</li>
+                <li>Groucho Marx</li>
+            </ul>
+            <h4 class="text-grey">The first words Mickey Mouse ever uttered in a cartoon were “hot dogs” (in “The
+                Karnival Kid, 1929). The cartoons he was featured in were silent ones up until that point.</h4>
+
+            <div class="gutter-top" ><a href="#" class="button roundcrnr">Next</a></div>
+        </div>
+
+    </div>
+    <div class="large-4 medium-4 columns gutter-top">
+        <div class="custom-panel bkgrd-liteyellow roundcrnr text-heavy text-center">
+            <h3 class="">Seriously Funny</h3>
+            <h4 class="text-hotdogred">Q: Where do you put a smart hot dog? </h4>
+            <h4 class="answer"> A: On the honor roll.
+            </h4>
+
+            <p class="gutter-top"><a href="#" class="medium button center bkgrd-green">Get more jokes!</a></p>
+        </div>
+    </div>
+</div>
+
+<!--END ADD NEW CONTENT HERE -->
+
+<!--start fatfootandsocial.php-->
+<?php include('fatfootandsocial.php'); ?>
+<!--end fatfootandsocial.php-->
+<!--end scripts included in fatfootsandsocial-->
+</html>
+
