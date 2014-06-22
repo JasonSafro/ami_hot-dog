07c14acbe001df4fd1d98b78ef839b47a194ce2a
Jason Safro <jacen6678@yahoo.com>
2014-06-22 17:43:03 -0400

    Added quiz CT, View, and template

---
+++ docroot/sites/all/themes/hotdogs/templates/nodes/node--quiz-question.tpl.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct url of the current node.
+ * - $terms: the themed list of taxonomy term links output from theme_links().
+ * - $display_submitted: whether submission information should be displayed.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the following:
+ *   - node: The current template type, i.e., "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined, e.g. $node->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $node->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ */
+
+// For some reason, array indexing is not consistent so we'll standardize before using
+$correct_answer = array();
+if( isset($node->field_correct_answer[LANGUAGE_NONE][0]) ) :
+  $correct_answer = $node->field_correct_answer[LANGUAGE_NONE];
+else :
+  $correct_answer = $node->field_correct_answer;
+endif;
+
+$incorrect_answer = array();
+if( isset($node->field_incorrect_answers[LANGUAGE_NONE][0]) ) :
+  $incorrect_answer = $node->field_incorrect_answers[LANGUAGE_NONE];
+else :
+  $incorrect_answer = $node->field_incorrect_answers;
+endif;
+
+// Build an array of possible answers
+$possible_answers = array();
+$possible_answers[] = array(
+  'answer' => $correct_answer[0]['value'],
+  'type' => 'correct'
+);
+foreach( $incorrect_answer as $one_answer ) :
+  $possible_answers[] = array(
+    'answer' => $one_answer['value'],
+    'type' => 'incorrect'
+  );
+endforeach;
+
+// Now sort it randomly
+shuffle($possible_answers);
+
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+	<?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+      <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  
+  <?php print render($content['field_question']); ?>
+  
+  <ul>
+  <?php foreach( $possible_answers as $one_answer ) : ?>
+    <li class="one-answer one-answer-<?php print $one_answer['type']; ?>" data-answer-type="<?php print $one_answer['type']; ?>" data-nid="<?php print $node->nid; ?>"><?php print $one_answer['answer']; ?></li>
+  <?php endforeach; ?>
+  </ul>
+  
+  <div class="reveal">
+    <div class="response" id="response-<?php print $node->nid; ?>"></div>
+    <div class="additional-information"><?php print render($content['field_additional_information']); ?></div>
+  </div>
+
+
+</article>
+
+<style>
+  .reveal {
+    display: none;
+  }
+</style>
+
+<script>
+  jQuery( document ).ready(function() {
+    jQuery('.one-answer').click(function() {
+      // Get the NID
+      var nid = jQuery(this).attr("data-nid");
+      
+      // Mark up the answers
+      jQuery('#node-' + nid + ' .one-answer-correct').css('color','green');
+      jQuery('#node-' + nid + ' .one-answer-incorrect').css('color','red');
+      
+      // Right or wrong
+      if( jQuery(this).attr('data-answer-type') == 'correct' ) {
+        jQuery('#response-' + nid).html('Right!');
+      } else {
+        jQuery('#response-' + nid).html('False!');
+      }
+      
+      // Reveal
+      jQuery('#node-' + nid + ' .reveal').css('display','block');
+    } );
+  });
+</script>
